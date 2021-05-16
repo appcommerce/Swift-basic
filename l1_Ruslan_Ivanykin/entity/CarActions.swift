6bf953fe92d3c8057291272c638f91b8a57fa34e
Ruslan Ivanykin <pandatek@inbox.ru>
2021-05-16 15:29:28 +0800

    Домашка

---
+++ l1_Ruslan_Ivanykin/entity/CarActions.swift
@@ -0,0 +1,17 @@
+//
+//  CarActions.swift
+//  l1_Ruslan_Ivanykin
+//
+//  Created by Руслан on 16.05.2021.
+//
+
+import Foundation
+
+enum CarActions{
+    case startEngine
+    case stopEngine
+    case openWindow
+    case closeWindow
+    case loadTrunk(Double)
+    case unloadTrunk(Double)
+}
--- l1_Ruslan_Ivanykin/main.swift
+++ l1_Ruslan_Ivanykin/main.swift
@@ -8,60 +8,16 @@
 import Foundation
 
 
-/*
- * Задание 1. Урок 2
- */
-func getEvenNumber(_ number: Int) -> Bool {
-    return number % 2 == 0
-}
-print(getEvenNumber(5))
+var lamborghini = SportCar(model: "Aventador", issue: 2000, capacity: 12.0, isStarting: false, isOpenWindows: false, maxCapacity: 12.0)
+var audi = SportCar(model: "R8", issue: 2018, capacity: 12.0, isStarting: false, isOpenWindows: false, maxCapacity: 14.0)
+var freightliner = TrunkCar(model: "eM2", issue: 2020, capacity: 0.0, isStarting: false, isOpenWindows: false, maxCapacity: 33_000.0)
 
-/*
- * Задание 2. Урок 2
- */
-func getRightDivForThree(_ number: Int) -> Bool {
-    return number % 3 == 0
-}
-print(getRightDivForThree(3))
+print(lamborghini)
+print(audi)
+print(freightliner)
 
-/*
- * Задание 3. Урок 2
- */
-var classicArray: [Int] = []
-//Заполняем массив
-func fillClassicArray(_ array: inout [Int]){
-    for i in 1...100 {
-        array.append(i)
-    }
-}
-fillClassicArray(&classicArray)
-print(classicArray)
-
-/*
- * Задание 4. Урок 2
- */
-//Убираем из массива все лишние числа.
-func deleteExcessNumbersFromArray(_ array: inout [Int]){
-    for i in array {
-        if let index = array.firstIndex(of: i) {
-            if getEvenNumber(i) || getRightDivForThree(i) {
-                array.remove(at: index)
-            }
-        }
-    }
-}
-deleteExcessNumbersFromArray(&classicArray)
-print(classicArray)
-
-//Задание 5. Урок 2
-var febArray: [Int] = [1, 1]
-func createFebArray(_ array: inout [Int]){
-    for i in 2...50 {
-        if i >= 2 {
-            let feb = array[i-2] + array[i-1]
-            array.append(feb)
-        }
-    }
-}
-createFebArray(&febArray)
-print(febArray)
+lamborghini.startContest(state: CarActions.startEngine)
+audi.startContest(state: CarActions.openWindow)
+freightliner.startContest(state: CarActions.loadTrunk(20_000.0))
+freightliner.startContest(state: CarActions.startEngine)
+freightliner.startContest(state: CarActions.unloadTrunk(34_000.0))
